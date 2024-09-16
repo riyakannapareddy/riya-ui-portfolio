a0677f425c168116c31a95b7cc95d512b93cbaca
Riya K <riyak4321@gmail.com>
2024-09-16 18:35:27 -0500

    Deploy website - based on 352ec49a96dc218197fdb163e6bfb770f426b0ab

---
+++ assets/js/814f3328.197fbf5e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Example Blog Post","permalink":"/riya-ui-portfolio/blog/example-blog-post","unlisted":false}]}')}}]);
