352ec49a96dc218197fdb163e6bfb770f426b0ab
Riya K <riyak4321@gmail.com>
2024-09-16 18:33:50 -0500

    deploy website
    https://riyakannapareddy.github.io/riya-ui-portfolio/

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,11 +7,11 @@ const config = {
   tagline: "Showcasing My Work and Thoughts",
   favicon: "img/favicon.ico",
 
-  url: "https://your-domain.com", // Replace with your actual domain
+  url: "https://riyakannapareddy.github.io/", // Replace with your actual domain
   baseUrl: "/",
 
-  organizationName: "your-org", // Replace with your GitHub org/user name
-  projectName: "my-ui-portfolio", // Replace with your repo/project name
+  organizationName: "riyakannapareddy", // Replace with your GitHub org/user name
+  projectName: "riya-ui-portfolio", // Replace with your repo/project name
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
