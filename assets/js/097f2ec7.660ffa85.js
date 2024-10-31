cee2f1ed6364bffd9e1dbeccdfce7493bef57149
riyakannapareddy <riyak4321@gmail.com>
2024-10-31 13:13:41 -0500

    Deploy website - based on b678ebaddd89c36ca6dfa83e1e9ecd83be801cf2

---
+++ assets/js/097f2ec7.660ffa85.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[7516],{6892:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/riya-ui-portfolio/blog/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":15,"previousPage":"/riya-ui-portfolio/blog","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
